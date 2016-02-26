06545c51d91e22d95bfff2914d6348c520028bae
Craig Giles <craig@gilesc.com>
2016-02-26 08:20:49 -0800

    removed comment from timestamps

--- commons/src/main/scala/com/gilesc/commons/Timestamps.scala
+++ commons/src/main/scala/com/gilesc/commons/Timestamps.scala
@@ -2,9 +2,6 @@ package com.gilesc.commons
 
 import java.time.LocalTime
 
-/**
-  * Created by gilesc on 11/28/15.
-  */
 trait Timestamps {
   val created_at: LocalTime
   val updated_at: LocalTime
