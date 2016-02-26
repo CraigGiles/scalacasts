cba8ec5a2ef1d810afc963de52af6a71d2ff73eb
Craig Giles <craig@gilesc.com>
2016-02-26 13:53:59 -0800

    added behavioral test scope

---
+++ backend/src/bt/scala/com/gilesc/scalacasts/MyIntegrationSpec.scala
@@ -0,0 +1,19 @@
+package com.gilesc.scalacasts
+
+import com.gilesc.commons.testing.TestCase
+
+class MyIntegrationSpec extends TestCase {
+  "Video Management" should {
+    "be able to manage a video" in {
+      given("video information is present")
+      // TODO:
+
+      when("The video is constructed")
+      // TODO:
+
+      then("It has the proper video meta information")
+      // TODO:
+
+    }
+  }
+}
--- project/BuildUtils.scala
+++ project/BuildUtils.scala
@@ -5,6 +5,8 @@ import com.gilesc._
 import com.gilesc.Scalaform
 
 object BuildUtils {
+  lazy val BehaviorTest = config("bt") extend(Test)
+
   val commonSettings = Seq(
     // Organization Settings
     organization := "com.gilesc",
@@ -17,11 +19,17 @@ object BuildUtils {
 
   lazy val rootProject = (project in file(".")).
     settings(commonSettings: _*)
+    .configs(IntegrationTest)
+    .configs(BehaviorTest)
+    .settings(inConfig(BehaviorTest)(Defaults.testSettings) : _*)
+    .settings(inConfig(IntegrationTest)(Defaults.itSettings) : _*)
 
   def createSubProject(name: String) = {
     Project(name, file(name))
     .configs(IntegrationTest)
-    .settings(Defaults.itSettings: _*)
+    .configs(BehaviorTest)
+    .settings(inConfig(BehaviorTest)(Defaults.testSettings) : _*)
+    .settings(inConfig(IntegrationTest)(Defaults.itSettings) : _*)
     .settings(commonSettings: _*)
   }
 }
