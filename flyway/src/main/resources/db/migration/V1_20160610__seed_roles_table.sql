8652f26f8084ffe7469ebd777c200e78d35d9fe8
Craig Giles <craig@gilesc.com>
2016-06-12 09:53:06 -0700

    New migrations for database

---
+++ flyway/src/main/resources/db/migration/V1_20160610__seed_roles_table.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO roles (name, description) VALUES
+    ('Customer', 'Consumer of the systems content'),
+    ('Screencast Producer', 'Producer of screencast content material'),
+    ('Member Support', 'Support staff for our members');
