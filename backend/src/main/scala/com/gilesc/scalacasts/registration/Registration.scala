06973973206d6d85c7fadcc1b9e485e71d173ded
Craig Giles <craig@gilesc.com>
2016-06-18 18:08:01 -0700

    Adding reader monad

--- backend/src/main/scala/com/gilesc/scalacasts/registration/Registration.scala
+++ backend/src/main/scala/com/gilesc/scalacasts/registration/Registration.scala
@@ -1,25 +1,69 @@
 package com.gilesc.scalacasts.registration
 
-import cats.data.Xor
+import cats.data.Reader
 import com.gilesc.scalacasts.User
 import com.gilesc.scalacasts.model.{Email, RawPassword, Username}
 import com.gilesc.security.password.PasswordHashing
 import org.mindrot.jbcrypt.BCrypt
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import scala.concurrent.Future
+
+trait UserRepo {
+  def insert(username: Username, email: Email, rawPassword: RawPassword): Future[User]
+  def find(username: Username): Future[Option[User]]
+}
+
+object InMemoryUserRepo {
+  var users = List.empty[User]
+}
+
+class InMemoryUserRepo extends UserRepo with PasswordHashing {
+  import InMemoryUserRepo._
+  override def insert(username: Username, email: Email, rawPassword: RawPassword): Future[User] = {
+    val usr = User(users.size + 1, username, email, hash(BCrypt.gensalt())(rawPassword))
+    users = usr :: users
+
+    Future(usr)
+  }
+
+  override def find(username: Username): Future[Option[User]] = Future(users.find(_.username == username))
+}
+
+trait RegistrationRepositories {
+  val user: UserRepo
+}
 
 trait Registration extends PasswordHashing {
   case class RegistrationContext(username: String, email: String, password: String)
   case class RegistrationError(messages: List[String])
 
-  val register: RegistrationContext => Xor[RegistrationError, User] = { cxt =>
+  def register(cxt: RegistrationContext) = Reader((repos: RegistrationRepositories) => {
     def strToRegistrationError(value: String): RegistrationError = RegistrationError(List[String](value))
-    val hashWithSalt = hash(BCrypt.gensalt())
 
     val user = for {
       username <- Username(cxt.username)
       email <- Email(cxt.email)
       rawPassword <- RawPassword(cxt.password)
-    } yield User(0, username, email, hashWithSalt(rawPassword))
+    } yield repos.user.insert(username, email, rawPassword)
 
     user.leftMap(strToRegistrationError)
-  }
+  })
+
+  //  val register: RegistrationContext => RegistrationRepositories => Xor[RegistrationError, User] = {
+  //    cxt: RegistrationContext =>
+  //    repos: RegistrationRepositories =>
+  //
+  //
+  //    def strToRegistrationError(value: String): RegistrationError = RegistrationError(List[String](value))
+  //    val hashWithSalt = hash(BCrypt.gensalt())
+  //
+  //    val user = for {
+  //      username <- Username(cxt.username)
+  //      email <- Email(cxt.email)
+  //      rawPassword <- RawPassword(cxt.password)
+  //    } yield User(0, username, email, hashWithSalt(rawPassword))
+  //
+  //    user.leftMap(strToRegistrationError)
+  //  }
 }
--- backend/src/test/scala/com/gilesc/scalacasts/model/RegistrationSpec.scala
+++ backend/src/test/scala/com/gilesc/scalacasts/model/RegistrationSpec.scala
@@ -1,21 +1,39 @@
 package com.gilesc.scalacasts.model
 
-import com.gilesc.scalacasts.registration.Registration
+import com.gilesc.scalacasts.registration.{InMemoryUserRepo, Registration, RegistrationRepositories}
 import com.gilesc.scalacasts.testing.TestCase
+import org.scalatest.concurrent.ScalaFutures
 
-class RegistrationSpec extends TestCase with Registration {
+class RegistrationSpec extends TestCase with ScalaFutures with Registration {
 
   "Registration System" should {
     "give back a user object after proper information is provided" in {
       val cxt = RegistrationContext("username", "email@test.com", "rawpassword")
-      val user = register(cxt)
-
-      user.isRight should be(true)
-      user.toOption.foreach { u =>
-        u.email.value should be(cxt.email)
-        u.username.value should be(cxt.username)
-        u.passwordHash.password should not be cxt.password
+      val repos = new RegistrationRepositories {
+        override val user = new InMemoryUserRepo
       }
+
+      val usr = for {
+        user <- register(cxt)(repos)
+      } yield user
+      //      val user = register(cxt)
+      //
+      usr.isRight should be(true)
+      usr map { u =>
+        whenReady(u) { user =>
+          user.email.value should be(cxt.email)
+          user.username.value should be(cxt.username)
+          user.passwordHash.password should not be cxt.password
+        }
+
+      }
+      //      whenReady(usr) { user =>
+      //        user.toOption.foreach { u =>
+      //          u.email.value should be(cxt.email)
+      //          u.username.value should be(cxt.username)
+      //          u.passwordHash.password should not be cxt.password
+      //        }
+      //      }
     }
   }
 
