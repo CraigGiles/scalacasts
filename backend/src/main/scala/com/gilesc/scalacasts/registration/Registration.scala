c04cfaf0e8955036261836af44bcb06e286531ad
Craig Giles <craig@gilesc.com>
2016-06-29 07:02:09 -0700

    removed hasher from registration

--- backend/src/main/scala/com/gilesc/scalacasts/registration/Registration.scala
+++ backend/src/main/scala/com/gilesc/scalacasts/registration/Registration.scala
@@ -4,7 +4,6 @@ import cats.data.Reader
 import com.gilesc.scalacasts.DatabaseBootstrap
 import com.gilesc.scalacasts.dataaccess.repository.UserRepository
 import com.gilesc.scalacasts.model.{Email, RawPassword, User, Username}
-import com.gilesc.security.password.PasswordHashing
 
 import scala.concurrent.Future
 
@@ -12,24 +11,24 @@ trait RegistrationRepositories {
   val user: UserRepository
 }
 
-trait Registration extends PasswordHashing with DatabaseBootstrap {
+trait Registration extends DatabaseBootstrap {
   case class RegistrationContext(username: String, email: String, password: String)
   case class RegistrationError(message: String) extends Exception(message)
 
   val register: RegistrationContext => Reader[RegistrationRepositories, Future[User]] = { cxt =>
-      Reader((repos: RegistrationRepositories) => {
-        def strToRegistrationError(value: String): RegistrationError = RegistrationError(value)
+    Reader((repos: RegistrationRepositories) => {
+      def strToRegistrationError(value: String): RegistrationError = RegistrationError(value)
 
-        val insertFunction = for {
-          username <- Username(cxt.username)
-          email <- Email(cxt.email)
-          rawPassword <- RawPassword(cxt.password)
-        } yield repos.user.insert(username, email, rawPassword)
+      val insertFunction = for {
+        username <- Username(cxt.username)
+        email <- Email(cxt.email)
+        rawPassword <- RawPassword(cxt.password)
+      } yield repos.user.insert(username, email, rawPassword)
 
-        insertFunction.toEither match {
-          case Left(error) => Future.failed(strToRegistrationError(error))
-          case Right(funct) => funct(db)
-        }
-      })
+      insertFunction.toEither match {
+        case Left(error) => Future.failed(strToRegistrationError(error))
+        case Right(funct) => funct(db)
+      }
+    })
   }
 }
