777f8a86e584ca866bddf14e35e4886a09254ed7
Craig Giles <craig@gilesc.com>
2016-06-30 06:46:07 -0700

    list of RolesRow to Seq[Role]

--- backend/src/main/scala/com/gilesc/scalacasts/dataaccess/repository/RoleRepository.scala
+++ backend/src/main/scala/com/gilesc/scalacasts/dataaccess/repository/RoleRepository.scala
@@ -2,6 +2,8 @@ package com.gilesc.scalacasts.dataaccess.repository
 
 import java.sql.Timestamp
 
+import com.gilesc.scalacasts.dataaccess.repository.RoleRepository.RoleId
+import com.gilesc.scalacasts.dataaccess.repository.UserRoles.Customer
 import com.gilesc.scalacasts.dataaccess.{Tables, DatabaseProfile}
 import com.gilesc.scalacasts.model.User
 import slick.driver.JdbcProfile
@@ -9,12 +11,18 @@ import slick.profile.SqlProfile.ColumnOption.SqlType
 
 import scala.concurrent.Future
 
+
 trait UserRoles {
-  sealed trait Role
-  final case object Customer extends Role
-  final case object ScreencastProducer extends Role
-  final case object MemberSupport extends Role
+  implicit val longToRoleId: Long => RoleId = id => RoleId(id)
+  implicit val intToRoleId: Int => RoleId = id => RoleId(id)
+  case class RoleId(underlying: Long) extends AnyVal
+
+  sealed case class Role(id: RoleId)
+  final case object Customer extends Role(1)
+  final case object ScreencastProducer extends Role(2)
+  final case object MemberSupport extends Role(3)
 }
+
 /**
   * CREATE TABLE IF NOT EXISTS `roles` (
   *   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
@@ -27,8 +35,6 @@ trait UserRoles {
   */
 class RoleRepository extends UserRoles {
   import com.gilesc.scalacasts.dataaccess.Tables._
-
-  case class RoleId(underlying: Long) extends AnyVal
 
   def insert(name: String, description: String): DatabaseProfile => Future[RolesRow] = { database =>
     import database._
@@ -47,40 +53,24 @@ class RoleRepository extends UserRoles {
     database.execute(Roles.result)
   }
 
+  def rolesRowToRole(row: RolesRow) = RoleId(row.id) match {
+    case Customer.id => Customer
+    case ScreencastProducer.id => ScreencastProducer
+    case MemberSupport.id => MemberSupport
+  }
+
   def getRolesForUser(user: User): DatabaseProfile => Future[Seq[Role]] = { database =>
     import database._
     import database.profile.api._
 
-//    val query = for {
-//      users <- Users filter(_.id == user.id)
-//      userRoles <- UserRoles if userRoles.userId == users.id
-//      roles <- Roles if roles.id == userRoles.roleId
-//    } yield (users, userRoles)
     val query = for {
       userRoles <- UserRoles filter(_.userId == user.id)
       roles <- Roles if roles.id == userRoles.roleId
-    } yield (roles)
+    } yield roles
 
     val something: Future[Seq[Tables.RolesRow]] = execute(query.result)
 
-    Future(Seq.empty[Role])
-
-    /**
-      * val query = for {
-      artist <- ArtistTable
-      album  <- AlbumTable if artist.id === album.artistId
-    } yield (artist, album)
-
-      */
-    //    Roles.join(UserRoles).on(_.id == _.roleId).
-//          join(Users).on(_._2.userId == _.id).
-//          filter { tables =>
-//          }
+    something map(_.map(rolesRowToRole))
   }
 
-  def getRoleId(role: Role): Future[RoleId] = role match {
-    case Customer => Future(RoleId(1))
-    case ScreencastProducer => Future(RoleId(2))
-    case MemberSupport => Future(RoleId(3))
-  }
 }
